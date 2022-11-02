7608773cab7920379f50368f0691b31166be311e
lucaspalomodevelop <lucas.palomo@t-online.de>
2022-11-02 18:57:34 +0100

    add some filemanipulation

--- cpp_1/main.cpp
+++ cpp_1/main.cpp
@@ -1,6 +1,43 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
-int main() {
-    std::cout << "Hello World!"<< std::endl;
+using namespace std;
+
+void readfile(string file); 
+void writefile(string file);
+
+int main()
+{
+    writefile("./testfile.txt");
+    readfile("./testfile.txt");
     return 0;
+}
+
+void readfile(string file)
+{
+    string line;
+    ifstream myfile;
+    myfile.open(file);
+    if (myfile.is_open())
+    {
+        while (getline(myfile, line))
+        {
+            cout << line << '\n';
+        }
+
+        myfile.close();
+    }
+}
+
+void writefile(string file)
+{
+    string line;
+    ofstream myfile;
+    myfile.open(file,std::ios_base::app);
+    if (myfile.is_open())
+    {
+        myfile << "hallo\n";
+    }
+    myfile.close();
 }
